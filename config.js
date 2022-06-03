d75f1e01e4f64a69ad17ebaf32347efc42d0d95d
hongxuyen2403 <106801660+hongxuyen2403@users.noreply.github.com>
2022-06-03 14:50:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,12 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'chÀo chó khốn nạn !',
+    introDesc: `xuyenpink`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Tao biết bánh này ngon nè',
+    desc: 'Ăn không tao đem qua🥟🥖🍲',
+    btnYes: 'không tao không ăn',
+    btnNo: 'Ăn sao không',
+    question:'chiều cn đi kvc ',
+    btnReply: 'Gửi con khồn'
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
